0753708164ee199030eb426c008e71d449ead74f
NahuelIglesias <nahueliglesias1999@gmail.com>
2022-10-19 20:20:33 -0300

    Added background

--- enviroment/objects/hurt_zone.gd
+++ enviroment/objects/hurt_zone.gd
@@ -6,3 +6,5 @@ func _ready():
 func _on_body_entered(body):
 	if body is Player:
 		body.notify_hit(-PlayerData.current_health)
+	elif body is Player2:
+		body.notify_hit(-Player2Data.current_health)
